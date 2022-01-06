6576fc1ac3ab2e320a19fe205a050a1c6b64715d
Aaron Murray <plugins@omv-extras.org>
2022-01-05 18:03:18 -0600

    add update_cert.sh

---
+++ update_cert.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+. /usr/share/openmediavault/scripts/helper-functions
+. /etc/default/openmediavault
+
+uuid="${1}"
+cert="${2}"
+key="${3}"
+
+if ! omv_isuuid "${uuid}"; then
+  echo "Invalid uuid"
+  exit 1
+fi
+
+if [ ! -f "${cert}" ]; then
+  echo "Cert not found"
+  exit 2
+fi
+
+if [ ! -f "${key}" ]; then
+  echo "Key not found"
+  exit 3
+fi
+
+# if you wanted to copy a cert & key to OMV location, this is how it would be done
+#cp -v "${cert}" "${OMV_SSL_CERTIFICATE_DIR}/certs/${OMV_SSL_CERTIFICATE_PREFIX}-${uuid}.crt"
+#cp -v "${key}" "${OMV_SSL_CERTIFICATE_DIR}/private/${OMV_SSL_CERTIFICATE_PREFIX}-${uuid}.key"
+
+echo "Cert file :: ${cert}"
+echo "Key file :: ${key}"
+
+xpath="/config/system/certificates/sslcertificate[uuid='${uuid}']"
+echo "xpath :: ${xpath}"
+echo
+
+if ! omv_config_exists "${xpath}"; then
+  echo "Config for ${uuid} does not exist"
+  exit 4
+fi
+
+echo "Updating certificate in database ..."
+omv_config_update "${xpath}/certificate" "$(cat ${cert})"
+
+echo "Updating private key in database ..."
+omv_config_update "${xpath}/privatekey" "$(cat ${key})"
+
+echo "Updating certs and nginx..."
+omv-salt deploy run certificates nginx
+
+systemctl restart nginx
+
+exit 0
